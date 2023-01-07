e0a6b11b76c89b548c4ca8c14eca5471598bc9a9
Yuliia Fedorychak <Yulyafed86@gmail.com>
2023-01-07 19:03:08 +0200

    Update contact.js

--- contactsSchema/contact.js
+++ contactsSchema/contact.js
@@ -6,4 +6,11 @@ const contactsSchema = Joi.object({
     phone: Joi.string().min(11).max(15).required(),
 });
 
+const contactsUpdateSchema = Joi.object({
+    name: Joi.string().alphanum().min(2).max(20),
+    email: Joi.string().min(2).max(20),
+    phone: Joi.string().min(11).max(15),
+});
+
+
 module.exports =  contactsSchema ;
