bfbfff582e9440e8d6fe57f6c8239900bf6513aa
Yuliia Fedorychak <Yulyafed86@gmail.com>
2023-01-15 21:46:13 +0200

    add changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 mongoose.set("strictQuery", false);
@@ -9,18 +10,41 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
-
 const { HOST_URI } = process.env;
 
 async function main() {
   try {
     await mongoose.connect(HOST_URI);
-    console.log("Connected to mongodb!");
+    console.log("Database connection successful");
+
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+
+    const schema = mongoose.Schema(
+      {
+        name: {
+          type: String,
+          required: [true, 'Set name for contact'],
+        },
+        email: {
+          type: String,
+        },
+        phone: {
+          type: String,
+        },
+        favorite: {
+          type: Boolean,
+          default: false,
+        },
+      }
+    );
+
+    const Contact = mongoose.model("contact", schema);
+     
+
   } catch (error) {
-    console.error("Error while connecting to mongodb", error.message);
+    console.error("Error connecting to Database", error.message);
     process.exit(1);
   }
 }
