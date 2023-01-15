88bf22417fd121c2d0f6766017a39f1b710055f9
Yuliia Fedorychak <Yulyafed86@gmail.com>
2023-01-15 16:03:49 +0200

    add mongoose

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
@@ -5,3 +12,17 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
 })
+
+const { HOST_URI } = process.env;
+
+async function main() {
+  try {
+    await mongoose.connect(HOST_URI);
+    console.log("Connected to mongodb!");
+  } catch (error) {
+    console.error("Error while connecting to mongodb", error.message);
+    process.exit(1);
+  }
+}
+
+main();
